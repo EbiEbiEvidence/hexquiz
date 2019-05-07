a284e28c71046b4c4d3816e27040ef544b47b549
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-07 13:00:40 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import chromedriver_binary
+from pathlib import Path
+from time import sleep
+from selenium.common.exceptions import NoAlertPresentException
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def main():
+    # Init driver
+    driver_options = Options()
+    driver_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=driver_options)
+
+    # Get login page
+    driver.get("https://lukerissacher.com/hexquiz")
+
+    score = 0
+    while True:
+        as_hex = driver.find_element_by_class_name("question").text.strip()
+        as_decimal = int(as_hex, 16)
+        as_binary = "{0:#b}".format(as_decimal)[2:]
+
+        driver.find_element_by_id("Answer").send_keys(as_binary)
+        driver.find_element_by_id("Submit").click()
+
+        sleep(1)
+        driver.save_screenshot("progress.png")
+        driver.find_element_by_id("Submit").click()
+
+        score += 1
+        sys.stdout.write("\rscore: {0}".format(score))
+
+
+if __name__ == "__main__":
+    main()
